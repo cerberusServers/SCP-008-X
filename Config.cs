b94ebce264fde7c3597620c75418e543f63280d2
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-08-05 20:20:15 -0500

    v1.1.0 Rebrand

--- Config.cs
+++ Config.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel;
 using Exiled.API.Interfaces;
-namespace Infection
+
+namespace SCP008X
 {
-    public saled class Config : IConfig
+    public sealed class Config : IConfig
     {
         public bool IsEnabled { get; set; } = true;
         public int InfectionChance { get; set; } = 100;
@@ -16,13 +17,7 @@ namespace Infection
         public bool CassieAnnounce { get; set; } = true;
         public string Announcement { get; set; } = "SCP 0 0 8 containment breach detected . Allremaining";
         public int ZombieDamage { get; set; } = 24;
-        public bool ZombiesInfect { get; set; } = true;
-        [Description("Should SCP-049-2 be allowed to commit suicide?")]
-        public bool ZombieSuicides { get; set; } = false;
         [Description("This is the text that will be displayed to SCP-049-2 players on revive and infection!")]
-        public string SuicideBroadcast { get; set; } = "SCP-049-2 is not allowed to suicide on this server!";
-        public bool PeanutInfects { get; set; } = false;
-        public bool DogInfects { get; set; } = false;
-        public int DogDamage { get; set; } = 48;
+        public string SuicideBroadcast { get; set; } = "";
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -2,23 +2,30 @@
 using Exiled.API.Features;
 using Exiled.API.Enums;
 using Exiled.Events.Handlers;
-using Handlers = Exiled.Events.Handlers;
+using Player = Exiled.Events.Handlers.Player;
+using Server = Exiled.Events.Handlers.Server;
 
-namespace Infection
+namespace SCP008X
 {
     public class Plugin : Plugin<Config>
     {
+        private static readonly Lazy<Plugin> LInstance = new Lazy<Plugin>(() => new Plugin());
+        public static Plugin Instance => LInstance.Value;
+        private Plugin()
+        {
+        }
+
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
         public override string Author { get; } = "DGvagabond";
-        public override string Name { get; } = "Infection";
-        public override Version Version { get; } = new Version(1, 0, 6);
+        public override string Name { get; } = "SCP-008-X";
+        public override Version Version { get; } = new Version(1, 1, 0);
         public override Version RequiredExiledVersion { get; } = new Version(2, 0, 10);
 
-        private Player PlayerEvents;
-        private Server ServerEvents;
+        private Handlers.Player PlayerEvents;
+        private Handlers.Server ServerEvents;
         public static Plugin Singleton;
-        
+
         public override void OnEnabled()
         {
             try
@@ -41,24 +48,24 @@ namespace Infection
         public void RegisterEvents()
         {
             Singleton = this;
-            PlayerEvents = new Player(this);
-            ServerEvents = new Server(this);
+            PlayerEvents = new Handlers.Player(this);
+            ServerEvents = new Handlers.Server(this);
 
-            Handlers.Player.Hurting += PlayerEvents.OnPlayerHurt;
-            Handlers.Player.Dying += PlayerEvents.OnPlayerDying;
-            Handlers.Player.ChangingRole += PlayerEvents.OnRoleChange;
-            Handlers.Player.MedicalItemUsed += PlayerEvents.OnHealing;
+            Player.Hurting += PlayerEvents.OnPlayerHurt;
+            Player.Dying += PlayerEvents.OnPlayerDying;
+            Player.ChangingRole += PlayerEvents.OnRoleChange;
+            Player.MedicalItemUsed += PlayerEvents.OnHealing;
             Scp049.StartingRecall += PlayerEvents.OnRecall;
-            Handlers.Server.RoundStarted += ServerEvents.OnRoundStart;
+            Server.RoundStarted += ServerEvents.OnRoundStart;
         }
         public void UnregisterEvents()
         {
-            Handlers.Player.Hurting -= PlayerEvents.OnPlayerHurt;
-            Handlers.Player.Dying -= PlayerEvents.OnPlayerDying;
-            Handlers.Player.ChangingRole -= PlayerEvents.OnRoleChange;
-            Handlers.Player.MedicalItemUsed -= PlayerEvents.OnHealing;
+            Player.Hurting -= PlayerEvents.OnPlayerHurt;
+            Player.Dying -= PlayerEvents.OnPlayerDying;
+            Player.ChangingRole -= PlayerEvents.OnRoleChange;
+            Player.MedicalItemUsed -= PlayerEvents.OnHealing;
             Scp049.StartingRecall -= PlayerEvents.OnRecall;
-            Handlers.Server.RoundStarted -= ServerEvents.OnRoundStart;
+            Server.RoundStarted -= ServerEvents.OnRoundStart;
 
             PlayerEvents = null;
             ServerEvents = null;
