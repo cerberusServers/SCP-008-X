da4ae8aa7937f195340109f5e2b905470b109181
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-08-15 03:07:00 -0500

    Added Commands

---
+++ Commands/Cure/All.cs
@@ -0,0 +1,44 @@
+﻿using CommandSystem;
+using RemoteAdmin;
+using Exiled.Permissions.Extensions;
+using Targets = Exiled.API.Features.Player;
+using CustomPlayerEffects;
+using System;
+
+namespace SCP008X.Commands.Cure
+{
+    class All : ICommand
+    {
+        public string Command { get; } = "all";
+
+        public string[] Aliases { get; } = { "*" };
+
+        public string Description { get; } = "Cure all players with SCP-008";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!(sender as PlayerCommandSender).CheckPermission("scp008.cure"))
+            {
+                response = "Missing permission: \"scp008.cure\"";
+                return false;
+            }
+
+            if (arguments.Count != 2)
+            {
+                response = "Usage: \"cure all\" or \"cure *\"";
+                return false;
+            }
+
+            foreach (Targets player in Targets.List)
+            {
+                if (player.Role.IsSCP(false))
+                    continue;
+
+                player.ReferenceHub.playerEffectsController.DisableEffect<Poisoned>();
+            }
+
+            response = "All human-class players have been cured.";
+            return true;
+        }
+    }
+}
