f8ceda02fd70464da3a9cc65da60cfb375687618
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-08-15 03:05:39 -0500

    Added Commands

---
+++ Commands/Infect/All.cs
@@ -0,0 +1,44 @@
+﻿using CommandSystem;
+using RemoteAdmin;
+using Exiled.Permissions.Extensions;
+using Targets = Exiled.API.Features.Player;
+using CustomPlayerEffects;
+using System;
+
+namespace SCP008X.Commands.Infect
+{
+    class All : ICommand
+    {
+        public string Command { get; } = "all";
+
+        public string[] Aliases { get; } = { "*" };
+
+        public string Description { get; } = "Infect all players with SCP-008";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!(sender as PlayerCommandSender).CheckPermission("scp008.infect"))
+            {
+                response = "Missing permission: \"scp008.infect\"";
+                return false;
+            }
+
+            if (arguments.Count != 2)
+            {
+                response = "Usage: \"infect all\" or \"infect *\"";
+                return false;
+            }
+
+            foreach (Targets player in Targets.List)
+            {
+                if (player.Role.IsSCP(false))
+                    continue;
+
+                player.ReferenceHub.playerEffectsController.EnableEffect<Poisoned>();
+            }
+
+            response = "All human-class players have been infected.";
+            return true;
+        }
+    }
+}
---
+++ Commands/Infect/Person.cs
@@ -0,0 +1,49 @@
+﻿using CommandSystem;
+using RemoteAdmin;
+using Exiled.Permissions.Extensions;
+using Target = Exiled.API.Features.Player;
+using CustomPlayerEffects;
+using System;
+
+namespace SCP008X.Commands.Infect
+{
+    class Person : ICommand
+    {
+        public string Command { get; } = "person";
+
+        public string[] Aliases { get; } = { "player", "user"};
+
+        public string Description { get; } = "Infect a player with SCP-008";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!(sender as PlayerCommandSender).CheckPermission("scp008.infect"))
+            {
+                response = "Missing permission: \"scp008.infect\"";
+                return false;
+            }
+
+            if (arguments.Count != 3)
+            {
+                response = "Usage: \"infect player (player id / name)\"";
+                return false;
+            }
+
+            Target player = Target.Get(arguments.At(0));
+            if (player == null)
+            {
+                response = $"Player \"{arguments.At(0)}\" not found";
+                return false;
+            }
+            else if (player.Role.IsSCP(false))
+            {
+                response = $"Player \"{arguments.At(0)}\" is not a valid class to infect";
+                return false;
+            }
+
+            player.ReferenceHub.playerEffectsController.EnableEffect<Poisoned>();
+            response = $"{player.Nickname} has been infected";
+            return true;
+        }
+    }
+}
