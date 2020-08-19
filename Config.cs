fb16d8f54926ffcd561b3e182595003e0f8de147
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-08-19 18:44:37 -0500

    New Inventory option

--- Config.cs
+++ Config.cs
@@ -18,5 +18,7 @@ namespace SCP008X
         public int ZombieDamage { get; set; } = 24;
         [Description("This is the text that will be displayed to SCP-049-2 players on revive and infection!")]
         public string SuicideBroadcast { get; set; } = "";
+        [Description("Should players keep their inventory after turning into a zombie? Items cannot be used by them.")]
+        public bool RetainInventory { get; set; } = true;
     }
 }
