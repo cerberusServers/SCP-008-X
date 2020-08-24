703097fc0d1ae734eb12d31e5993581498d547ce
DGvagabond <54526517+DGvagabond@users.noreply.github.com>
2020-08-23 23:53:35 -0500

    Changes
    Added new option for hints

--- Config.cs
+++ Config.cs
@@ -18,6 +18,8 @@ namespace SCP008X
         public int ZombieDamage { get; set; } = 24;
         [Description("This is the text that will be displayed to SCP-049-2 players on revive and infection!")]
         public string SuicideBroadcast { get; set; } = "";
+        [Description("Text displayed to players after they've been infected")]
+        public string InfectionAlert { get; set; } = "";
         [Description("Should players keep their inventory after turning into a zombie? Items cannot be used by them.")]
         public bool RetainInventory { get; set; } = true;
     }
