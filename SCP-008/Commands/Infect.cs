d1227bd8446eb42d2cc678cd95851630c8ab5d17
Franco <francoquiroz_13@live.com>
2021-02-11 22:32:16 -0300

    Nice plugin DGvagabond

---
+++ SCP-008/Commands/Infect.cs
@@ -0,0 +1,56 @@
+﻿using RemoteAdmin;
+using CommandSystem;
+using System;
+using Exiled.API.Features;
+using UnityEngine;
+using Exiled.Permissions.Extensions;
+using SCP008X.Components;
+
+namespace SCP008X.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class Infect : ICommand
+    {
+        public string Command { get; } = "infect";
+
+        public string[] Aliases { get; } = null;
+
+        public string Description { get; }="Forcefully infect a player with SCP-008";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (!((CommandSender)sender).CheckPermission("scp008.infect"))
+            {
+                response = "Missing permissions.";
+                return false;
+            }
+            Player ply = Player.Get(arguments.At(0));
+            if(ply == null)
+            {
+                response = "Invalid player.";
+                return false;
+            }
+            switch (ply.Team)
+            {
+                case Team.SCP:
+                    response = "You can not infect SCP players.";
+                    return false;
+                case Team.TUT:
+                    response = "You can not infect this class.";
+                    return false;
+                case Team.RIP:
+                    response = "You can not infect the dead.";
+                    return false;
+            }
+            if(ply.ReferenceHub.TryGetComponent(out SCP008 s008))
+            {
+                response = "This player is already infected.";
+                return false;
+            }
+            ply.ReferenceHub.gameObject.AddComponent<SCP008>();
+            ply.ShowHint($"<color=yellow><b>SCP-008</b></color>\n{SCP008X.Instance.Config.InfectionAlert}", 10f);
+            response = $"{ply.Nickname} has been infected.";
+            return true;
+        }
+    }
+}
---
+++ SCP-008/Config.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace SCP008X
+{
+    public sealed class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        [Description("Only enable this if you're looking for bug sources!")]
+        public bool DebugMode { get; set; }
+        [Description("Display plugins stats at the end of the round?")]
+        public bool SummaryStats { get; set; }
+        public int InfectionChance { get; set; } = 40;
+        public int CureChance { get; set; } = 60;
+        [Description("Toggle players getting infected via area of effect")]
+        public bool AoeInfection { get; set; } = false;
+        [Description("Set AOE infection to run when infected players turn?")]
+        public bool AoeTurned { get; set; } = false;
+        [Description("Set the percentage chance players will get infected by area of effect")]
+        public int AoeChance { get; set; } = 50;
+        [Description("Allow SCP-049 to instantly revive targets?")]
+        public bool BuffDoctor { get; set; } = false;
+        public int ZombieHealth { get; set; } = 385;
+        [Description("How much AHP should be given to Zombies?")]
+        public int Scp008Buff { get; set; } = 30;
+        public int MaxAhp { get; set; } = 150;
+        public bool CassieAnnounce { get; set; } = true;
+        public string Announcement { get; set; } = "SCP 0 0 8 containment breach detected . Allremaining";
+        public int ZombieDamage { get; set; } = 45;
+        [Description("This is the text that will be displayed to SCP-049-2 players on spawn")]
+        public string SuicideBroadcast { get; set; } = "<color=red> Fuiste completamente consumido por el SCP-008, ahora cuando ataques a tus enemigos hay una probabilidad de que los infectes</color>";
+        [Description("Text displayed to players after they've been infected")]
+        public string InfectionAlert { get; set; } = "<color=red>[</color><b><color=green>Plaga Zombie</color></b><color=red>]</color> fuiste infectado por el <color=red>SCP-008</color> curate con un medikit o con un SCP-500 para eliminar la infeccion!!";
+        [Description("Text displayed to newly turned SCP-049-2 players")]
+        public string SpawnHint { get; set; } = "<color=yellow>Los jugadores que ataques seran infectados con el</color> SCP-008</color><color=yellow>!</colot>";
+        [Description("Should players keep their inventory after turning into a zombie? Items cannot be used by them.")]
+        public bool RetainInventory { get; set; } = true;
+    }
+}
